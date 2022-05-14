a8291e750e32a38f3510af336ff3b5e75bc9bfbb
Flunx <hookoux@gmail.com>
2022-05-14 20:39:18 +0300

    Initial Commit

---
+++ Planetarii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Planetarii
+//
+//  Created by admin on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
